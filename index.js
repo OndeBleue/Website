f4480c86f182af3bed2474a36c0bde9835b5c9b7
Sylvain <bond.never.die@gmail.com>
2019-03-07 20:17:27 +0100

    fixed about-us support for IE&Edge

---
+++ index.js
@@ -0,0 +1,20 @@
+// scroll to anchors
+new SmoothScroll('a[href*="#"]', {
+  offset: 130
+});
+
+// show/hide small devices menu
+function toggleMenu() {
+  const isShown = (document.getElementById('menu').style.display === 'flex');
+  if (isShown) {
+    document.getElementById('menu').style.display = 'none';
+  } else {
+    document.getElementById('menu').style.display = 'flex';
+  }
+}
+
+// google analytics
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-8020731-9');
