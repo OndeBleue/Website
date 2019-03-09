72419e10bacb058519ff248e5fc099cd8aaaf0e6
Sylvain <bond.never.die@gmail.com>
2019-03-09 13:57:42 +0100

    accept/refuse cookies

--- index.js
+++ index.js
@@ -15,19 +15,36 @@ function toggleMenu() {
 
 // google analytics
 window.dataLayer = window.dataLayer || [];
+window['ga-disable-UA-8020731-9'] = hasOptOutGA();
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'UA-8020731-9');
-window['ga-disable-UA-8020731-9'] = hasOptOutGA();
 
 function gaOptOut(event) {
   event.preventDefault();
   event.stopPropagation();
 
-  document.cookie = "ga-disable-UA-8020731-9=true;max-age=31536000";
+  document.cookie = "refuse-cookies=true;max-age=31536000";
   window['ga-disable-UA-8020731-9'] = true;
+  hideCookiesInfo();
+}
+
+function gaAccept() {
+  document.cookie = "refuse-cookies=false;max-age=31536000";
+  hideCookiesInfo();
 }
 
 function hasOptOutGA() {
-  return document.cookie.replace(/(?:(?:^|.*;\s*)ga-disable-UA-8020731-9\s*\=\s*([^;]*).*$)|^.*$/, "$1") === 'true';
+  return document.cookie.replace(/(?:(?:^|.*;\s*)refuse-cookies\s*\=\s*([^;]*).*$)|^.*$/, "$1") === 'true';
 }
+
+function hideCookiesInfo() {
+  document.getElementById("cookies-info").style.display = 'none';
+}
+
+// when DOM is ready, display cookies info if no choices were made before
+document.addEventListener("DOMContentLoaded", function() {
+  if (document.cookie.split(';').filter((item) => item.trim().startsWith('refuse-cookies=')).length === 0) {
+    document.getElementById("cookies-info").style.display = 'block';
+  }
+});
